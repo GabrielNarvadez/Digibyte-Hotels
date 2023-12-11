90679cb3ae951c36a5ae548848a3814aac91c5dd
GabrielNarvadez <gab.narvadez@gmail.com>
2023-12-11 10:36:00 +0800

    Final but logo

--- main/serializers.py
+++
@@ -1,7 +0,0 @@
-import serializers
-from .models import Amenitie, Room, StandardRoom, FamilyRoom, DeluxeRoom
-
-class RoomSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = StandardRoom
-        fields = ['room_number', 'amenitie', 'occupied', 'paid']
--- main/views.py
+++ main/views.py
@@ -1,49 +1,10 @@
-from django.shortcuts import get_object_or_404, render
-from django.http import JsonResponse
-from .models import Amenitie, Room, StandardRoom, FamilyRoom, DeluxeRoom
-
-from django.views.decorators.csrf import csrf_exempt
-
-API_KEY = "django-digibyte-hotels-key"
+from django.shortcuts import render
 
 # Create your views here.
 def home_screen_view(request):
-    return render(request, "personal/home.html")
 
-def update_vacancy_status(room_number, occupied=True):
-    try:
-        room = Room.objects.get(room_number=room_number)
-        room.occupied = occupied  
-        room.save() 
-        return True
-    except Room.DoesNotExist:
-        return False
-    
-def update_room_status(request, room_number, token):
-    if token_is_valid(token):
-        success = update_vacancy_status(room_number, occupied=True)
-        if success:
-            response_data = {
-                'status': 'success',
-                'message': 'Room status updated successfully.',
-                'data': {'room_number': room_number, 'occupied': True}
-            }
-            return JsonResponse(response_data, status=200)
-        else:
-            response_data = {
-                'status': 'error',
-                'message': 'Room not found.',
-            }
-            return JsonResponse(response_data, status=404)
-    else:
-        response_data = {
-            'status': 'error',
-            'message': 'Invalid Token.',
-        }
-        return JsonResponse(response_data, status=403)
-
-def token_is_valid(token):
-    return token == API_KEY
+    context = {}
+    return render(request, "personal/home.html", context)
 
 def about_screen_view(request):
 
@@ -69,7 +30,7 @@ def facility_screen_view(request):
 
     return render(request, "facility.html", context)
 
-def rooms_overview_screen_view(request):
+def room_overview_screen_view(request):
 
     context = {}
 
